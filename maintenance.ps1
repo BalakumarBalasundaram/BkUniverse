7a3dd14a36687e058c3d64baf5d57f652aa51d7e
Balakumar Balasundaram <balakumar.b05@gmail.com>
2023-04-01 17:34:02 +0200

    Update maintenance.ps1

--- maintenance.ps1
+++ maintenance.ps1
@@ -15,3 +15,19 @@ function Get-FolderSize {
  }
  
  Get-FolderSize ('C:\Users\')
+ 
+ 
+ 
+ C:\> $targetfolder='C:\'
+ C:\> $dataColl = @()
+ C:\> gci -force $targetfolder -ErrorAction SilentlyContinue | ? { $_ -is [io.directoryinfo] } | % {
+ $len = 0
+ gci -recurse -force $_.fullname -ErrorAction SilentlyContinue | % { $len += $_.length }
+ $foldername = $_.fullname
+ $foldersize= '{0:N2}' -f ($len / 1Gb)
+ $dataObject = New-Object Object
+ Add-Member -inputObject $dataObject -memberType NoteProperty -name “foldername” -value $foldername
+ Add-Member -inputObject $dataObject -memberType NoteProperty -name “foldersizeGb” -value $foldersize
+ $dataColl += $dataObject
+ }
+ C:\> $dataColl | Out-GridView -Title “Size of subdirectories”
